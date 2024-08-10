4057632d52bb10753dcaac668652a7f4b8e65d1d
Jubin <jubinphilip25@gmail.com>
2024-08-10 11:33:12 +0530

    git commit

--- contact.js
+++ contact.js
@@ -1,11 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
     const form = document.querySelector('.contactform');
+    const nameBox=document.querySelector('#nameBox');
+    const emailBox=document.querySelector('#emailBox');
+    const phoneBox=document.querySelector('#phoneBox');
+    const msgBox = document.querySelector('#msgBox');
 
     form.addEventListener('submit', function(event) {
       
         const name = form.querySelector('#name').value.trim();
     if (name === '') {
-        alert('Please enter your name.');
+       nameBox.textContent='Please enter your name.'
         event.preventDefault();
         return;
     }
@@ -13,7 +17,7 @@ document.addEventListener("DOMContentLoaded", function() {
     // Check if name contains only alphabets
     const namePattern = /^[A-Za-z\s]+$/;
     if (!namePattern.test(name)) {
-        alert('Name should only contain alphabets.');
+        nameBox.textContent="Name should only contain alphabets"
         event.preventDefault();
         return;
     }
@@ -22,7 +26,7 @@ document.addEventListener("DOMContentLoaded", function() {
         const email = form.querySelector('#email').value.trim();
         const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
         if (!emailPattern.test(email)) {
-            alert('Please enter a valid email address.');
+            emailBox.textContent='Please enter a valid email address.'
             event.preventDefault();
             return;
         }
@@ -31,7 +35,7 @@ document.addEventListener("DOMContentLoaded", function() {
         const phone = form.querySelector('#phone').value.trim();
         const phonePattern = /^\d{10}$/;
         if (!phonePattern.test(phone)) {
-            alert('Please enter a valid 10-digit phone number.');
+            phoneBox.textContent='Please enter a valid 10-digit phone number.'
             event.preventDefault();
             return;
         }
@@ -39,7 +43,7 @@ document.addEventListener("DOMContentLoaded", function() {
      
         const message = form.querySelector('#message').value.trim();
         if (message === '') {
-            alert('Please enter your message.');
+            msgBox.textContent='Please enter your message.'
             event.preventDefault();
             return;
         }
